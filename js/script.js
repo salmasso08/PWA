4f4dad852da3f5fe773dcde80d12b36eeb1ff0be
Allan Salmasso <salmasso08@gmail.com>
2019-05-22 22:33:50 -0300

    atualizado 22/05

--- js/script.js
+++ js/script.js
@@ -1,4 +1,49 @@
-let notes = {data: []};
+let notes = window.localStorage.getItem('notes') || '{"data": []}';
+notes = JSON.parse(notes);
+let updateList = function(){
+    console.log('[Application start watch]')
+
+    Array.observe(notes.data, function(changes){
+        let index = null;
+        let value = '';
+        let status = null;
+
+        if (changes[0].type === 'splice'){
+            index = changes[0].index;
+            value = changes[0].object[index];
+            status = (changes[0].addedCount > 0 ) ? 'created' : 'removed' ;
+        }
+
+        if (changes[0].type === 'update'){
+            index = changes[0].name;
+            value = changes[0].object[index];
+            status = updated; 
+        }
+
+        if (!value && status === 'created' && status === 'updated'){
+            return;
+        }
+
+        let notesTag = document.getElementById('notes');
+
+        if(status === 'update'){
+            console.log('implementar')
+        }
+
+        if(status === 'removed'){
+            let listOffNotes = document.querySelectorAll('#notes li');
+            notesTag.removeChild(listOffNotes[index]);
+        }
+
+        if(status === 'created'){
+            let newLi = document.createElement('li');
+            newLi.innerHTML = value
+            notesTag.appendChild(newLi);
+        }
+        window.localStorage.setItem('notes', JSON.stringify(notes));
+
+    });
+}
 
 let createNote = function(){
     let input = document.querySelector('#form-add-note input[type="text"]');
@@ -7,7 +52,15 @@ let createNote = function(){
     input.value = "";
 }
 
+updateList();
 document.addEventListener('DOMContentLoaded', function(event){
+    let listOffNotes = document.getElementById('notes');
+    let listHtml = '';
+
+    for (let i=0; i<notes.data.length; i++){
+        listHtml += '<li>' + notes.data[i] + '</li>';
+    }
+    listOffNotes.innerHTML = listHtml;
     let formAddNotes = document.getElementById('form-add-note');
     formAddNotes.addEventListener('submit',function(e){
         e.preventDefault();
