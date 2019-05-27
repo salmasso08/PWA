806bc4f0d214d9965926891e7afbf783b7820656
asalmasso <allansalmasso@sbt.com.br>
2019-05-27 20:39:08 -0300

    ^^

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,13 @@
-let cacheName = 'notes-son-v.1.0.0';
+let cacheName = 'notes-son';
 let filesToCache = [
     './',
     'index.html',
     'css/colors.css',
-    'css/styles.css',
-    'js/array.observe.polyfill.js',
+    'css/style.css',
+    'js/array.obeserve.polyfill.js',
     'js/object.observe.polyfill.js',
-    'js/scripts.js'
+    'js/script.js',
+    'service-worker.js'
 ];
 
 self.addEventListener('install', function (e) {
@@ -19,25 +20,25 @@ self.addEventListener('install', function (e) {
     );
 });
 
-self.addEventListener('activate', function (e) {
-    console.log('[ServiceWorker] Activate');
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function(key) {
-                if (key !== cacheName) {
-                    console.log('[ServiceWorker] Removing old cache', key);
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
-});
+// self.addEventListener('activate', function (e) {
+//     console.log('[ServiceWorker] Activate');
+//     e.waitUntil(
+//         caches.keys().then(function (keyList) {
+//             return Promise.all(keyList.map(function(key) {
+//                 if (key !== cacheName) {
+//                     console.log('[ServiceWorker] Removing old cache', key);
+//                     return caches.delete(key);
+//                 }
+//             }));
+//         })
+//     );
+// });
 
-self.addEventListener('fetch', function (e) {
-    console.log('[ServiceWorker] Fetch', e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
-});
+// self.addEventListener('fetch', function (e) {
+//     console.log('[ServiceWorker] Fetch', e.request.url);
+//     e.respondWith(
+//         caches.match(e.request).then(function(response) {
+//             return response || fetch(e.request);
+//         })
+//     );
+// });
