f30e039c072996b527fe9caf0b821fa78bc96e0d
Allan Salmasso <salmasso08@gmail.com>
2019-05-27 21:56:59 -0300

    upload final

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-let cacheName = 'notes-son';
+let cacheName = 'notes-sson';
 let filesToCache = [
     './',
     'index.html',
@@ -20,25 +20,25 @@ self.addEventListener('install', function (e) {
     );
 });
 
-// self.addEventListener('activate', function (e) {
-//     console.log('[ServiceWorker] Activate');
-//     e.waitUntil(
-//         caches.keys().then(function (keyList) {
-//             return Promise.all(keyList.map(function(key) {
-//                 if (key !== cacheName) {
-//                     console.log('[ServiceWorker] Removing old cache', key);
-//                     return caches.delete(key);
-//                 }
-//             }));
-//         })
-//     );
-// });
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
 
-// self.addEventListener('fetch', function (e) {
-//     console.log('[ServiceWorker] Fetch', e.request.url);
-//     e.respondWith(
-//         caches.match(e.request).then(function(response) {
-//             return response || fetch(e.request);
-//         })
-//     );
-// });
+self.addEventListener('fetch', function (e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
